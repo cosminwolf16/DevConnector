fb70bf104771f381c789e604b11ab3de4fafd022
Smokyn <smokyn_cosmin@yahoo.com>
2020-03-18 16:47:35 +0200

    removed unsued model

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,7 +5,6 @@ const auth = require("../../middleware/auth");
 
 const Post = require("../../models/Post");
 const User = require("../../models/User");
-const Profile = require("../../models/Profile");
 
 // @route  POST api/posts
 // @desc   Create a post
