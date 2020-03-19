24c19ff94eb1a70eede7ae849cd7106442635fe4
Smokyn <smokyn_cosmin@yahoo.com>
2020-03-19 11:40:02 +0200

    ...

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
+app.get("/", (req, res) => {
+  res.send(`API Running...`);
+});
 
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
