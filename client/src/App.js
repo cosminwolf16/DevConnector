e2208564b4af0310aa362a1d16f7799f4f036fd8
Smokyn <smokyn_cosmin@yahoo.com>
2020-03-23 15:12:06 +0200

    comit letter from big to small

--- client/src/App.js
+++ client/src/App.js
@@ -32,7 +32,7 @@ const App = () => {
             <Alert />
             <Switch>
               <Route exact path='/register' component={Register} />
-              <Route exact path='/Login' component={Login} />
+              <Route exact path='/login' component={Login} />
             </Switch>
           </section>
         </Fragment>
